75dd576beb90e7b4a30463303c2233554a668671
mseckykoebel <mseckykoebel@gmail.com>
2020-01-28 09:48:34 -0600

    fxed the formatting

--- project/__main__.py
+++ project/__main__.py
@@ -1,16 +1,15 @@
 import sys
 import helpers
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-	if len(sys.argv) != 2:
-		print("Wrong number of arguments, make sure to include your data file. ie. python3 project ./yourdata.json")
-		sys.exit(2)
+    if len(sys.argv) != 2:
+        print(
+            "Wrong number of arguments, make sure to include your data file. ie. python3 project ./yourdata.json"
+        )
+        sys.exit(2)
 
-	path = sys.argv[1]
+    path = sys.argv[1]
 
-	helpers.comment_extract_file(path)
-	print("Opening file " + path)
-
-
-
+    helpers.comment_extract_file(path)
+    print("Opening file " + path)
--- project/helpers.py
+++ project/helpers.py
@@ -1,30 +1,32 @@
 import json
 
+
 def comment_extract_file(path):
-	""" 
+    """ 
 	Helper function to extract comments from JSON tweets.
 	Arguments : JSON file directory (Tweets)
 	Output	  : List of comments strings
 	"""
 
-	with open(path, "r") as read_file:
-		data = json.load(read_file)
+    with open(path, "r") as read_file:
+        data = json.load(read_file)
 
-	comments = comment_extract(data)
-	
-	return comments
+    comments = comment_extract(data)
+
+    return comments
+
 
 def comment_extract(json_obj):
-	""" 
+    """ 
 	Helper function to extract comments from JSON obj.
 	Arguments : JSON Obj of tweets
 	Output	  : List of comments strings
 	"""
 
-	comment_list = []
+    comment_list = []
 
-	for comment in json_obj:
-		text = comment if type(comment) is str else comment["text"]
-		comment_list.append( text )
+    for comment in json_obj:
+        text = comment if type(comment) is str else comment["text"]
+        comment_list.append(text)
 
-	return comment_list	
+    return comment_list
