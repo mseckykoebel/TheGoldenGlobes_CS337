bad2529ca66a42b2d9efb6bccf4470596d2d4342
mseckykoebel <mseckykoebel@gmail.com>
2020-02-08 11:26:21 -0600

    finished pre-processing and gathering subset of actors names, and works

--- gg_api.py
+++ gg_api.py
@@ -1,43 +1,33 @@
-# required
+#!/usr/bin/env python
+# required and not required
 import nltk
 import numpy
-#import spacy
-
-# not required
 import json
-# to see how long the program has been running
+import re
+import statistics
+import sys
 import time
+import gzip
+import ssl
+# opens IMDb url
+import urllib.request
+from collections import Counter
+# compare hashable sentences
+from difflib import SequenceMatcher
+# spacy and the spacy tokenizer
+import spacy
+from spacy.tokenizer import Tokenizer
+spacy.prefer_gpu()
+nlp = spacy.load("en_core_web_sm")
 
-# other files
-import winnerFromNominees
-
-# prefer the use of GPU for a neural network
-#spacy.prefer_gpu()
-#nlp = spacy.load("en_core_web_sm")
-
-# get the name of the project members
-class TeamMember:
-
-    def __init__(self, name, netID):
-        self.name = name
-        self.netID = netID
-
-    def _get_name(self):
-        print(self.name)
-
-member1 = TeamMember("Mason Secky-Koebel", "msf9197")
-# TODO
-member2 = TeamMember("", "")
-member3 = TeamMember("", "")
-member4 = TeamMember("", "")
-
-def getTeamMembers():
-    print('Team members: ')
-    print(member1._get_name(), member2._get_name(), member3._get_name(), member4._get_name())
+# helper functions
+from helpers import getIMDbData
+from helpers import getTeamMembers
 
 
 """Version 0.35"""
 
+# pre-defined award names
 OFFICIAL_AWARDS_1315 = [
     "cecil b. demille award",
     "best motion picture - drama",
@@ -94,6 +84,92 @@ OFFICIAL_AWARDS_1819 = [
     "best performance by an actor in a supporting role in a series, limited series or motion picture made for television",
     "cecil b. demille award",
 ]
+OFFICIAL_AWARDS_2020 = None
+
+### Defining program constants
+AWARD_TOKEN_SET = set()
+# possible keywords for the ceremoney itself
+AWARD_KEYWORDS = ["#goldenglobes","goldenglobes2013","goldenglobes2015","golden globes","golden","globes","gg2013","gg2015","gg2020"]
+
+# all of the names in the IMDb database are going to go here
+nameDictionary = {}
+
+def pre_ceremony():
+    """This function loads/fetches/processes any data your program
+    will use, and stores that data in your DB or in a json, csv, or
+    plain text file. It is the first thing the TA will run when grading.
+    Do NOT change the name of this function or what it returns."""
+
+    """Creates a tsv file of the names of all of the actors on the
+    IMDb database, their birth and death year, primary profession, and"""
+
+    global nameDictionary
+    
+    print("Beginning the pre-ceremoney process...")
+    # TIMER START
+    timer = time.time()
+
+    f = getIMDbData()
+
+    print("Processing data to nameDictionary (data.tsv cam be opened with excel")
+    print("\n")
+
+    f = gzip.open('nameBasics.tsv.gz')
+    # read the file as strings
+    dataContent = str(f.read())
+    # split the content where there is a new line
+    dataSeparators = dataContent.split('\\n')
+    # split the lines with tab
+    for line in dataSeparators:
+        # all of the data will be in the array
+        allData = []
+        allData.append(line.split('\\t'))
+    # only take names from 2012 - 2020 (assuming this is not tested earlier)
+    for year in range(2012, 2020):
+        nameDictionary[str(year)] = []
+
+    # ignore the first and last line
+    for name in allData[1:len(allData)-1]:
+        # get the name, birth, and death fields
+        actualActorName = name[1]
+        actorBirthday = name[2]
+        actorDeathDay = name[3]
+
+        # if data in the table is missing, pass over it and move onto the next name
+        if actorBirthday == '\\N':
+            continue
+        if actualActorName == '\\N':
+            continue
+
+        # check if they're still alive
+        if actorDeathDay == '\\N':
+            # object of all years the actor has been active, in order
+            yearsActive = range(int(actorBirthday), 2020)
+        else:
+            yearsActive = range(int(actorBirthday), int(actorDeathDay) + 1)
+
+        # check edge cases
+        if yearsActive[0] < 2010 and yearsActive[-1] < 2010:
+            continue
+        if yearsActive[0] > 2020:
+            continue
+        if yearsActive[0] < 2010:
+            yearsActive = range(2010, yearsActive[-1]+1)
+
+        # add names to the data file, and not the other information
+        for year in yearsActive:
+            nameDictionary[str(year)].append(actualActorName)
+
+    print("Pre-ceremony processing complete.")
+
+    print("\n")
+
+    # TIMER END
+    print("Total runtime: %s seconds" % str(time.time() - timer))
+
+    print("\n")
+
+    return
 
 
 def get_hosts(year):
@@ -134,16 +210,6 @@ def get_presenters(year):
     return presenters
 
 
-def pre_ceremony():
-    """This function loads/fetches/processes any data your program
-    will use, and stores that data in your DB or in a json, csv, or
-    plain text file. It is the first thing the TA will run when grading.
-    Do NOT change the name of this function or what it returns."""
-    # Your code here
-    print("Pre-ceremony processing complete.")
-    return
-
-
 def main():
     """This function calls your program. Typing "python gg_api.py"
     will run this function. Or, in the interpreter, import gg_api
@@ -151,6 +217,10 @@ def main():
     run when grading. Do NOT change the name of this function or
     what it returns."""
     return
+
+# run these before main
+getTeamMembers()
+pre_ceremony()
 
 if __name__ == "__main__":
     # elapsedSeconds = seconds since 0
---
+++ helpers.py
@@ -0,0 +1,100 @@
+# required and not required
+import nltk
+import numpy
+import json
+import re
+import statistics
+import sys
+import time
+import gzip
+import ssl
+# opens IMDb url
+import urllib.request
+from collections import Counter
+# compare hashable sentences
+from difflib import SequenceMatcher
+
+# get the name of the project members
+class TeamMember:
+
+    def __init__(self, name, netID):
+        self.name = name
+        self.netID = netID
+
+    def _get_name(self):
+        return self.name
+
+member1 = TeamMember("Mason Secky-Koebel", "msf9197")
+member2 = TeamMember("", "")
+member3 = TeamMember("", "")
+member4 = TeamMember("", "")
+
+def getTeamMembers():
+    print('Team members: \n')
+    print(member1._get_name(), member2._get_name(), member3._get_name(), member4._get_name())
+    print("\n")
+
+def extractText(json_obj):
+    """ 
+	Helper function to extract comments from JSON obj.
+	Arguments : JSON Obj of tweets
+	Output	  : List of comments strings
+	"""
+    comment_list = []
+    for comment in json_obj:
+        text = comment if type(comment) is str else comment["text"]
+        comment_list.append(text)
+
+    return comment_list
+
+def extractTextFromFile(path):
+    """ 
+	Helper function to extract comments from JSON tweets.
+	Arguments : JSON file directory (Tweets)
+	Output	  : List of comments strings
+	"""
+
+    with open(path) as read_file:
+        data = json.load(read_file)
+
+    comments = extractText(data)
+    print(comments)
+    return comments
+
+# function that downloads the IMBd database name data
+def getIMDbData():
+    # this can be opened with excel, or VSCode
+    ssl._create_default_https_context = ssl._create_unverified_context
+    # download names from IMDB about (https://datasets.imdbws.com/)
+    print("\n")
+    print("Downloading name.basics.tsv.gz from https://datasets.imdbws.com/")
+    print("\n")
+    # download the file and store as nameBasics
+    urllib.request.urlretrieve('https://datasets.imdbws.com/name.basics.tsv.gz', 'nameBasics.tsv.gz')
+    print("Download complete!")
+    print("\n")
+
+    # make and return the file
+    file = gzip.open('nameBasics.tsv.gz')
+    return file
+
+# function that can take in json tweet file, and optional
+# second argument for number of tweets wanted
+def getTweets(tweetFile, numOfTweets):
+
+    # open the tweet file
+    with open(tweetFile) as read_file:
+        tweets = json.load(read_file)
+
+    # get the second argument
+    numberOfTweets = int(numOfTweets)
+
+    # take a random subset of the number of tweets entered
+    tweets = random.sample(tweets, numberOfTweets)
+
+    # take only text
+    tweetText = []
+    for i in tweets:
+        tweetText.append(i["text"])
+
+    return tweetText
